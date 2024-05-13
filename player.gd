13d2c3158b9d3844d92de1820ce613c810cacddd
Raffozo <rafaellegendofdark@hotmail.com>
2024-05-13 20:22:44 -0300

    First Commit

---
+++ player.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+@export var speed = 10.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Hello World! from Player")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	position.x += input.x * speed * delta
+	position.y += input.y * speed * delta
